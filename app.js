cfc0963263106eb210c300f393d1a145c5ada5d5
thaypcosta <117128583+thaypcosta@users.noreply.github.com>
2024-12-19 17:49:25 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+alert('Boas vindas ao nosso site!');
+let nome = 'Lua';
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+alert('Erro! Preencha todos os campos');
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+alert('mensagemDeErro');
+nome = prompt('Qual seu nome?');
+idade = prompt('Qual sua idade?');
+if(idade>=18){ 
+   alert('Pode tirar a habilitação') 
+};
